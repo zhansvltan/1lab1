45c6f50120c592f727b897ef52b99e5c323296f1
zhansvltan <assemzhar@icloud.com>
2024-03-05 18:28:13 +0500

    home page

--- src/page/Add.jsx
+++ src/page/Add.jsx
@@ -55,7 +55,7 @@ export default function Add() {
           onChange={handleSurnameChange}
           type="text"
           name="surname"
-          placeholder="Last Name"
+          placeholder="Surname"
         />
         <input
           value={phone}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,19 @@
 import "./styles.css";
 import List from "./List";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigateTo = useNavigate();
+  const toList = () => {
+    navigateTo("/list");
+  };
+  const toAdd = () => {
+    navigateTo("/add");
+  };
   return (
     <nav className="navigation-menu">
-      <List />
+      <button onClick={toList}> Go to list</button>
+      <button onClick={toAdd}>Go to add form</button>
     </nav>
   );
 }
